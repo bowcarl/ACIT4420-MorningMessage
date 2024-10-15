1252d382a0c5a69731fcaa76c67752b982776f52
Carl Petter Mørch-Reiersen <35300302+bowcarl@users.noreply.github.com>
2024-10-15 09:40:23 +0200

    Update logger.py

--- morning_greetings/logger.py
+++ morning_greetings/logger.py
@@ -1,5 +1,6 @@
 import datetime
 def log_message(contact, message):
+    '''Log a message to a file'''
     with open("message_log.txt", "a") as log_file:
         log_file.write(f"{datetime.datetime.now()} - Send to {contact}['name']: {message}\n")
 
