3be952c9d9cb182d2ebec8795f757414c5fd7324
Carl Petter Mørch-Reiersen <35300302+bowcarl@users.noreply.github.com>
2024-10-16 16:47:23 +0200

    Update message_generator.py

--- morning_greetings/message_generator.py
+++ morning_greetings/message_generator.py
@@ -1,3 +1,6 @@
 def generate_message(name):
+    if not name:
+        raise ValueError("Name is missing")
     '''Generate a morning greeting message'''
     return f"Good Morning, {name}! Have a wonderful day!"
+
