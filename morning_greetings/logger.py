0a55994ee8627c020b8c2f358ae48204944bf7fa
Carl Petter Mørch-Reiersen <35300302+bowcarl@users.noreply.github.com>
2024-10-16 16:46:57 +0200

    Update logger.py

--- morning_greetings/logger.py
+++ morning_greetings/logger.py
@@ -2,7 +2,8 @@ import datetime
 import os
 def log_message(contact, message):
     '''Log a message to a file'''
+    if not contact:
+        raise ValueError("Contact is missing")
     file_path = os.path.join(os.path.dirname(__file__), 'message_log.txt') # Variable to store the path of message_log.txt
     with open(file_path, "a") as log_file:
         log_file.write(f"{datetime.datetime.now()} - Send to {contact}['name']: {message}\n")
-
